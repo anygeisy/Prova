b3ec0fcc0bd8d57a9f225ad315ad87dd8439ce89
Geisy Any Lima da Silva <anygeisy@hotmail.com>
2021-04-18 15:24:01 -0400

    Adicionar arquivos de projeto.

---
+++ WebApiProva1/Controllers/CidadaoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppiAula1.Models;
+
+namespace WebApiProva1.Controllers
+{
+    public class CidadaoController : ApiController
+    {
+        // GET: api/Cidadao
+        public List<Cidadao> Get()
+        {
+            Cidadao cidadao = new Cidadao();
+            return cidadao.listaCidadao();
+        }
+
+        // GET: api/Cidadao/5
+        public Cidadao Get(string Gnome)
+        {
+            Cidadao cidadao = new Cidadao();
+            return cidadao.listaCidadao().Where(x => x.Gnome == Gnome).FirstOrDefault();
+        }
+
+        // POST: api/Cidadao
+        public List<Cidadao> Post(Cidadao cidadao)
+        {
+            Cidadao _cidadao = new Cidadao();
+            _cidadao.Inserir(cidadao);
+            return _cidadao.listaCidadao();
+        }
+
+        // PUT: api/Cidadao/5
+        public List<Cidadao> Put(int Gid, Cidadao cidadao)
+        {
+            Cidadao _cidadao = new Cidadao();
+            _cidadao.Atualizar(Gid, cidadao);
+            return _cidadao.listaCidadao();
+        }
+
+        // DELETE: api/Cidadao/5
+        public void Delete(int Gid)
+        {
+            Cidadao _cidadao = new Cidadao();
+            _cidadao.Deletar(Gid);
+        }
+    }
+}
---
+++ WebApiProva1/Models/Cidadao.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace WebAppiAula1.Models
+{
+    public class Cidadao
+    {
+        public int Gid { get; set; }
+        public string Gnome { get; set; }
+        public string Gtelefone { get; set; }
+        public string Gemail { get; set; }
+        public int Gidade { get; set; }
+
+        public List<Cidadao> listaCidadao()
+        {
+
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = File.ReadAllText(caminhoArquivo);
+            var listaCidadao = JsonConvert.DeserializeObject<List<Cidadao>>(json);
+            return listaCidadao;
+
+        }
+       
+        public bool ReescreverArquivo(List<Cidadao> listaCidadao)
+        {
+            var caminhoArquivo = HostingEnvironment.MapPath(@"~/App_Data\Base.json");
+            var json = JsonConvert.SerializeObject(listaCidadao, Formatting.Indented);
+            File.WriteAllText(caminhoArquivo, json);
+            return true;
+
+        }
+
+        public Cidadao Inserir(Cidadao Cidadao)
+        {
+            var ListaCidadao = this.listaCidadao();
+            var maxId = ListaCidadao.Max(cidadao => cidadao.Gid);
+            Cidadao.Gid = maxId + 1;
+            ListaCidadao.Add(Cidadao);
+            ReescreverArquivo(ListaCidadao);
+            return Cidadao;
+        }
+        
+        public Cidadao Atualizar(int Gid, Cidadao Cidadao)
+        {
+            var ListaCidadao = this.listaCidadao();
+            var itemIndex = ListaCidadao.FindIndex(p => p.Gid == Gid);
+            if (itemIndex >= 0)
+            {
+                Cidadao.Gid = Gid;
+                ListaCidadao[itemIndex] = Cidadao;
+
+            }
+            else
+            {
+                return null;
+
+            }
+            ReescreverArquivo(ListaCidadao);
+            return Cidadao;
+        }
+        
+        public bool Deletar(int Gid)
+        {
+            var ListaCidadao = this.listaCidadao();
+            var itemIndex = ListaCidadao.FindIndex(p => p.Gid == Gid);
+            if (itemIndex >= 0)
+            {
+                ListaCidadao.RemoveAt(itemIndex);
+
+            }
+            else
+            {
+                return false;
+            }
+            ReescreverArquivo(ListaCidadao);
+            return true;
+        }
+    }
+}
